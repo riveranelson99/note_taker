81bac87939ea8646b3ef20b076fa0e714fd8a524
Nelson Rivera <riveranelson99@gmail.com>
2021-10-26 19:15:51 -0500

    Added functionality of new notes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const notes = require('./db/db.json')
+const uuid = require('./public/assets/helpers/uuid')
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -16,6 +18,30 @@ app.get('/', (req,res) =>
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => 
+    res.json(notes)
+);
+
+
+app.post('/api/notes', (req, res) => {
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuid(),
+    }
+
+    notes.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes,null, 2));
+    res.json(notes);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    notes = notes.filter(note => note.id !== req.params.id);
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes, null, 2));
+    res.json(notes);
+});
 
 app.listen(PORT, () => 
     console.log(`App listening at http://localhost:${PORT}`)
